fc7ce3dceb4d91d0646b44b721628f968902830d
kelvindaudi <kelvindaudi98@gmail.com>
2023-06-20 11:18:33 -0700

    k

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,17 +1,19 @@
 #include "main.h"
-#include <stdio.h>
 /**
- * print_alphabet - main function
- *
- * Return: returns void
+ * main -Entry point
+ * description: prints out putchar to stdout
+ * Return: always 0
  */
-void print_alphabet(void)
+int main(void)
 {
-	char z;
-
-	for (z = 'a'; z <= 'z'; z++)
-	{
-		_putchar(z);
-	}
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 	_putchar('\n');
+	return (0);
 }
