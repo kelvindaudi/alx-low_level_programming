3eb57bd97d64332023851fb51e1605e1db588a32
kelvindaudi <kelvindaudi98@gmail.com>
2023-06-21 08:47:41 -0700

    k

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,21 +1,19 @@
 #include "main.h"
 /**
- * main - check the code.
- *
- * Return: Always 0.
+ * _isalpha - shows 1 if the input is a letter,
+ * another case, shows 0
+ * @c
+ * Return: 1 for letters, return 0 for the rest
  */
-int main(void)
+int _isalpha(int c)
 {
-	int r;
-
-	r = _isalpha('H');
-	_putchar(r + '0');
-	r = _isalpha('o');
-	_putchar(r + '0');
-	r = _isalpha(108);
-	_putchar(r + '0');
-	r = _isalpha(';');
-	_putchar(r + '0');
+	if ((c >= 97 && c <= 122) || (c >= 65 && c <= 90))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
 	_putchar('\n');
-	return (0);
 }
