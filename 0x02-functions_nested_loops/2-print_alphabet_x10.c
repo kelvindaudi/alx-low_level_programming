f2438229fe418dc1a2b121cce7220c85d7260771
kelvindaudi <kelvindaudi98@gmail.com>
2023-06-21 05:03:05 -0700

    k

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * print_alphabet_x10 - prints 10 times the alphabet, in lowercase
- * return: always 0 (success)
+ * Return: always 0 (success)
  */
 void print_alphabet_x10(void)
 {
