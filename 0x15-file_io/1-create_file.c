f47f9d16629debd68b91fb705d0b025681903dd1
kelvindaudi <kelvindaudi98@gmail.com>
2023-10-30 04:07:24 -0700

    file io

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,42 +1,28 @@
 #include "main.h"
-#include <stdlib.h>
-#include <fcntl.h>
 
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <unistd.h>
-
-#include <stddef.h>
 /**
- * create_file - creates a text file.
- *
- * @filename: name of file to do I/O operations on.
- * @text_content: NULL terminated string to write to file created.
- * Return: 1 on success, -1 on failure.
+ * create_file - a function that creates a file
+ * @filename: file pointer
+ * @text_content: The string pointer
+ * Return: returns 0 on succesful execution
  */
-
 int create_file(const char *filename, char *text_content)
 {
-	ssize_t count;
-	int fd, index;
+	int fd, w, len = 0;
 
 	if (filename == NULL)
 		return (-1);
 
-	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
-
-	if (fd == -1)
-		return (-1);
-
 	if (text_content != NULL)
 	{
-		for (index = 0; text_content[index]; index++)
-			;
+		for (len = 0; text_content[len];)
+			len++;
 	}
 
-	count = write(fd, text_content, index);
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	w = write(fd, text_content, len);
 
-	if (count == -1)
+	if (fd == -1 || w == -1)
 		return (-1);
 
 	close(fd);
--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,42 +1,31 @@
 #include "main.h"
-#include <stdlib.h>
-#include <fcntl.h>
 
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <unistd.h>
-
-#include <stddef.h>
 /**
- * append_text_to_file - appends text at the end of a file.
- *
- * @filename: name of file to do I/O operations on.
- * @text_content: NULL terminated string to write to file created.
- * Return: 1 on success, -1 on failure.
+ * append_text_to_file - a function that ppends a text
+ * @filename: the file pointer
+ * @text_content: appending string
+ * Return: returns 0 on succesful exection
  */
-
 int append_text_to_file(const char *filename, char *text_content)
 {
-	ssize_t count;
-	int fd, index;
+	int o, w, len = 0;
 
 	if (filename == NULL)
 		return (-1);
 
 	if (text_content != NULL)
 	{
-		for (index = 0; text_content[index]; index++)
-			;
+		for (len = 0; text_content[len];)
+			len++;
 	}
 
-	fd = open(filename, O_WRONLY | O_APPEND | O_CREAT, S_IRUSR | S_IWUSR);
+	o = open(filename, O_WRONLY | O_APPEND);
+	w = write(o, text_content, len);
 
-	count = write(fd, text_content, index);
-
-	if (fd == -1 || count == -1)
+	if (o == -1 || w == -1)
 		return (-1);
 
-	close(fd);
+	close(o);
 
 	return (1);
 }
